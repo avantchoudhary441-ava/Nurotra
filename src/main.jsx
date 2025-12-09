7556a39f065d365daec3e0f673248fb75d0ea770
ANUJ <tanuj1284@gmail.com>
2025-12-09 14:49:46 +0530

    modified:   package.json 	modified:   src/main.jsx 	modified:   vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/global.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+
 
 // 🔥 Load saved theme BEFORE React renders (important for no flash)
 const savedTheme = localStorage.getItem("theme") || "light";
@@ -10,8 +11,8 @@ document.documentElement.setAttribute("data-theme", savedTheme);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
