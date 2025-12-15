9e07943f0b6205bc47cc44c62fa08f39f67496d0
ANUJ <tanuj1284@gmail.com>
2025-12-15 10:25:45 +0530

    modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/Nurotra/",   
+  plugins: [react()]
 });
 
 
