f68801327c2de5f437eb685a3902c9db4e18cebf
avantchoudhary441-ava <avantchoudhary341@gamil.com>
2026-01-05 23:34:47 +0530

    modified:   src/pages/match/MatchResultPage.jsx

--- src/pages/match/MatchResultPage.jsx
+++ src/pages/match/MatchResultPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { motion } from "framer-motion";
 import "../../styles/matchResult.css";
 import logo from "../../assets/NurotraLogo.png";
 import BackgroundEffects from "../../components/BackgroundEffects";
