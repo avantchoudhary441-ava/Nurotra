49e5711ee9fd0e04cce5a41c762f72f758865af3
ANUJ <tanuj1284@gmail.com>
2026-01-17 15:27:50 +0530

    modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -87,7 +87,7 @@ require("./socket/socketHandler")(io);
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../dist")));
-    app.get("(.*)", (req, res) => {
+    app.get("/:path*", (req, res) => {
         res.sendFile(path.resolve(__dirname, "../", "dist", "index.html"));
     });
 }
